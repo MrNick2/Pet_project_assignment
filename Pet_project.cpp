5b8f08235b83c7e68cc5119b40918ec1257ad82d
Kovacs Gergo <90850070+MrNick2@users.noreply.github.com>
2026-05-07 00:14:18 +0200

    Second commit

--- Pet_project.cpp
+++ Pet_project.cpp
@@ -1,6 +1,44 @@
+// For input and output
+#include <iostream>
+
+// So I don't have to write std everywhere
 using namespace std;
 
-int main() {
+class Countdown {     // The class
+  public:           // Access specifier
+    Countdown(int countdown_start) {     // Constructor
+      cout << "Hello World!";
+      countdown_number = countdown_number;
+    }
+
+    private:
+    int countdown_number;
+    void The_Countdown_Thing()
+    {
+        
+    }
+};
+
+int main()
+{
+    int countdown_start;
+
+    cout<<"--------------"<<endl;
+    cout<<"Timer program!"<<endl;
+    cout<<"--------------"<<endl;
+
+    cout<<"Give me the seconds that you want the countdown to start from: ";
+    cin>>countdown_start;
+
+    Countdown My_Countdown(countdown_start);
+
+    system("clear");
+
+    for (size_t i = countdown_start; i > 0; i--)
+    {
+        cout<<""
+    }
     
+
     return 0;
 }
