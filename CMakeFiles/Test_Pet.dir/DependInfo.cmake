e398c5f5c6cb255f3d9e3d6674f0f1c37ae6ded9
Kovacs Gergo <90850070+MrNick2@users.noreply.github.com>
2026-05-07 13:28:00 +0200

    After really final correction (CMake things)

---
+++ CMakeFiles/Test_Pet.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/Seagate vegyes/----------------EGYETEM DOLGOK(MSC GYŐR)/Production Software Development/Beadandó/Console_Display.cpp" "CMakeFiles/Test_Pet.dir/Console_Display.cpp.o" "gcc" "CMakeFiles/Test_Pet.dir/Console_Display.cpp.o.d"
+  "/mnt/Seagate vegyes/----------------EGYETEM DOLGOK(MSC GYŐR)/Production Software Development/Beadandó/Countdown.cpp" "CMakeFiles/Test_Pet.dir/Countdown.cpp.o" "gcc" "CMakeFiles/Test_Pet.dir/Countdown.cpp.o.d"
+  "/mnt/Seagate vegyes/----------------EGYETEM DOLGOK(MSC GYŐR)/Production Software Development/Beadandó/Real_Delay.cpp" "CMakeFiles/Test_Pet.dir/Real_Delay.cpp.o" "gcc" "CMakeFiles/Test_Pet.dir/Real_Delay.cpp.o.d"
+  "/mnt/Seagate vegyes/----------------EGYETEM DOLGOK(MSC GYŐR)/Production Software Development/Beadandó/Test_Pet_project.cpp" "CMakeFiles/Test_Pet.dir/Test_Pet_project.cpp.o" "gcc" "CMakeFiles/Test_Pet.dir/Test_Pet_project.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
